29af9eb82f835b6c00507d17a8d5bfde84f16c92
Dheeraj Kumar Rana(UST <272799@ust.com>
2026-02-02 12:30:41 +0530

    2nd initial commit

--- src/controllers/orderController.js
+++ src/controllers/orderController.js
@@ -2,22 +2,43 @@ const Order = require('../models/Order');
 const product = require('../models/Product');
 
 exports.createOrder = async (req, res) => {
+    try{
     const {productId, quantity, customerId} = req.body;
+
+    const lastOrder = await Order.find().sort({_id : -1}).limit(1).select("_id");
+
+    const nextId = lastOrder[0]._id ? lastOrder[0]._id + 1 : 1;
+
     const product = await Product.findById(productId);
+
+    if(!product || !product.isAvailable){
+        return res.status(400).json({message: 'Product Not Found'});
+    };
+    
     if(product.quantity < quantity){
         return res.status(400).json({message: 'Insufficient product quantity'});
     };
 
     const total = product.price * quantity;
     const order = await Order.create({
-        productId,
+        _id : nextId,
+        productId : product._id,
         quantity,
         price: product.price,
-        total,
-        customerId
+        totalAmount : total,
+        customerId,
+        orderDate : new Date()
     });
-    product.quantity -= quantity;
 
+    product.quantity -= quantity;
     await product.save();
+
     res.status(201).json(order);
+    } catch(error){
+    res.status(500).json({message : error.message});
+    }
 };
+
+exports.getAllOrders = async (req, res)=>{
+    
+}
--- src/models/Order.js
+++ src/models/Order.js
@@ -1,6 +1,7 @@
 const moongoose = require("mongoose");
 
 const orderSchema = new moongoose.Schema({
+    _id : Number,
     productId : {type: moongoose.Schema.Types.ObjectId, ref: 'Product'},
     quantity : Number,
     price : Number,
@@ -8,6 +9,6 @@ const orderSchema = new moongoose.Schema({
     status : {type: String, enum: ['CREATED', 'CANCELLED', 'COMPLETED'], default: 'CREATED'},
     customerId : {type: moongoose.Schema.Types.ObjectId, ref: 'User'},
     orderDate : Date
-}, { timestamps: true });
+}, { timestamps: true, _id: false });
 
 module.exports = moongoose.model("Order", orderSchema);
